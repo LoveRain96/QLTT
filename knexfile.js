4dacadf97de46b140eee4d101df6822fea78b8af
tienryo <tienvv@sphinx-software.com>
2018-07-06 11:07:17 +0700

    config circle ci

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     development: {
         client: 'mysql',
         connection: {
-            host: 'localhost',
+            host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
             password: process.env.DB_PASS || '123456',
             database: process.env.DB_DATA || 'QLTT',
